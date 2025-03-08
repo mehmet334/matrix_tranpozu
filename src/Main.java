e96d82d1e283418c10d1af561e573f3b9099a954
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-08 23:28:11 +0300

    ilk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Matrix boyutludur satır sayisi: ");
+        int satır = scanner.nextInt();
+        System.out.println("Matrix boyutludur kolon sayisi: ");
+        int kolon= scanner.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[][] matrix = new int[satır][kolon];
+
+        for (int i = 0; i < satır; i++) {
+            for (int j = 0; j < kolon; j++) {
+                System.out.println("[" +i +","+j+"] Elemanı giriniz : ");
+                int sayi= scanner.nextInt();
+                matrix[i][j]= sayi;
+                sayi=0;
+            }
+        }
+        System.out.println("Girdiğiniz matrix....");
+        for (int i = 0; i < satır; i++) {
+            for (int j = 0; j < kolon; j++) {
+                System.out.print(matrix[i][j]+ " ,");
+            }
+            System.out.println("");
+        }
+        System.out.println("Matrix'in transpozu.....");
+
+        int[][] matrixtranspoz =new int[kolon][satır];
+        for (int i = 0; i < satır; i++) {
+            for (int j = 0; j < kolon; j++) {
+                matrixtranspoz[j][i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < kolon; i++) {
+            for (int j = 0; j < satır; j++) {
+                System.out.print(matrixtranspoz[i][j] + ",");
+            }
+            System.out.println("");
         }
     }
 }
